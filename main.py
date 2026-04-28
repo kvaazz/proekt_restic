bb28ad61e800f789be071b8f5a10fe002384462e
Mikhail <mikhain@mail.ru>
2026-04-28 21:20:15 +0300

    работает

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from data.Forms import LoginForm, RegisterForm
 from flask_restful import reqparse, abort, Api, Resource
 from data.users import User
 from data import users_resources
+import requests
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -30,6 +32,87 @@ def load_user(user_id):
 @app.route('/')
 def page():
     return render_template('first_page.html')
+
+
+@app.route('/location', methods=['GET', 'POST'])
+def location():
+    api_server = 'https://static-maps.yandex.ru/v1?'
+    toponym_longitude = 31.253898
+    toponym_lattitude = 58.525216
+    API_KEY = "c7e6930c-2621-45c2-9bf7-3b85675e054c"
+
+    params = {
+        "apikey": API_KEY,
+        "ll": ",".join([str(toponym_longitude), str(toponym_lattitude)]),
+        "pt": ",".join([str(toponym_longitude), str(toponym_lattitude), str('pm2dbm')]),
+        'z': '13'
+    }
+
+    response = requests.get(api_server, params=params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(response)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+
+    map_file = "static/upload/map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    if request.method == 'POST':
+        geocode = request.form.get('address')
+        if not geocode:
+            print('Пожалуйста, введите адрес', 'error')
+            return redirect(url_for('location'))
+        print(f"Адрес пользователя: {geocode}")
+        server_address = 'http://geocode-maps.yandex.ru/1.x/?'
+        api_key = 'c514a804-086f-4788-9357-4ac645d35c74'
+
+        geocoder_request = f'{server_address}apikey={api_key}&geocode=Великий Новгород,{geocode}&format=json'
+
+        response = requests.get(geocoder_request)
+        if not response:
+            geocoder_request = f'{server_address}apikey={api_key}&geocode=Новгородская область,{geocode}&format=json'
+            response = requests.get(geocoder_request)
+        if response:
+            json_response = response.json()
+
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+
+            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+
+            toponym_coodrinates = toponym["Point"]["pos"]
+
+            print(toponym_address, "имеет координаты:", toponym_coodrinates)
+            toponym_longitude1 = toponym_coodrinates.split()[0]
+            toponym_lattitude1 = toponym_coodrinates.split()[1]
+            print(toponym_longitude1, toponym_lattitude1)
+            api_server = 'https://static-maps.yandex.ru/v1?'
+            toponym_longitude = 31.253898
+            toponym_lattitude = 58.525216
+
+            API_KEY = "c7e6930c-2621-45c2-9bf7-3b85675e054c"
+            pt = ",".join([str(toponym_longitude), str(toponym_lattitude), str('pm2dbm')]) + '~' + ",".join(
+                [str(toponym_longitude1), str(toponym_lattitude1), str('pm2wtm')])
+            print(pt)
+            params = {
+                "apikey": API_KEY,
+                "ll": ",".join([str(toponym_longitude), str(toponym_lattitude)]),
+                "pt": ",".join([str(toponym_longitude), str(toponym_lattitude), str('pm2dbm')]) + '~' + ",".join(
+                [str(toponym_longitude1), str(toponym_lattitude1), str('pm2wtm')]),
+            }
+
+            response = requests.get(api_server, params=params)
+
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print(response)
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+
+            map_file = "static/upload/map.png"
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+    return render_template('location.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
